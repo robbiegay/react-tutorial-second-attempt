88e791006e50031154d2460348c26979371b192e
Robbie Gay <robbiecgay@gmail.com>
2019-10-24 09:59:19 -0400

    Updates

---
+++ precache-manifest.75ee7268f22bd994a00c650282712da6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8355526a1d84f10f5f4745a6310cbbe",
+    "url": "/react-tutorial-second-attempt/index.html"
+  },
+  {
+    "revision": "d666a55f73823b515d37",
+    "url": "/react-tutorial-second-attempt/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "fd0af71ca4f01c906de0",
+    "url": "/react-tutorial-second-attempt/static/js/2.31e54e4c.chunk.js"
+  },
+  {
+    "revision": "d666a55f73823b515d37",
+    "url": "/react-tutorial-second-attempt/static/js/main.a32cf16d.chunk.js"
+  },
+  {
+    "revision": "e9292aedfaf38d5b3243",
+    "url": "/react-tutorial-second-attempt/static/js/runtime-main.8d7059bd.js"
+  }
+]);
